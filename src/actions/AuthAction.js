0193d736164fa910539e66548cfb56c11f96783c
ramankumar12995 <ramankumar12995@gmail.com>
2023-04-29 16:00:18 +0530

    added all the things for login and signup

--- src/actions/AuthAction.js
+++ src/actions/AuthAction.js
@@ -4,7 +4,7 @@ export const logIn = (formData) => async (dispatch) => {
     dispatch({ type: "AUTH_START" });
     try {
         const { data } = await AuthApi.logIn(formData);
-        dispatch({type: "AUTH_SUCCESS"})
+        dispatch({type: "AUTH_SUCCESS", data: data})
     } catch (error) {
         console.log(error)
         dispatch({type:"AUTH_FAIL"})
@@ -15,7 +15,7 @@ export const signUp = (formData) => async (dispatch) => {
     dispatch({ type: "AUTH_START" })
     try {
         const { data } = await AuthApi.signUp(formData)
-        dispatch({type:"AUTH_SUCCESS"})
+        dispatch({type:"AUTH_SUCCESS", data: data})
     } catch (error) {
         console.log(error)
         dispatch({type: "AUTH_FAIL"})
