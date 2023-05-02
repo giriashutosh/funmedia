19f3edc3bf41e2ee6e4d47e9ce6630ce7c0a39d8
ramankumar12995 <ramankumar12995@gmail.com>
2023-05-02 16:04:34 +0530

    updated post and profile working on infocard, somethings left to update in the postreducer like getTimelInepost and likepost

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,15 +1,34 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from '../ProfileModal/ProfileModal';
-
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import * as UserApi from '../../api/UserRequest'
 
 const InfoCard = () => {
     const [modalOpened, setModalOpened] = useState(false)
+    const profileUserId = useParams();
+    const user = useSelector((state) => state.authReducer.auth)
+    const [profileUser, setProfileUser] = useState({})
+
+    useEffect(() => {
+        const fetchProfileUser = async () => {
+            if (profileUserId === user._id) {
+                setProfileUser(user)
+            } else {
+                console.log('Fetching')
+                const profileUser = await UserApi.getUser((profileUserId))
+                setProfileUser(profileUser)
+            }
+        }
+        
+    })
+
   return (
     <div className='Infocard'>
           <div className='infoHead'>
-              <h4>Your Info</h4>
+              <h4>Profile Info</h4>
           
              <div>
                 <UilPen width="2rem" height="1.2rem" onClick = {()=>setModalOpened(true)}/>
@@ -20,13 +39,13 @@ const InfoCard = () => {
               <span>
                   <b>Status </b>
               </span>
-              <span>in Relationship </span>
+              <span>{ profileUser.relationship}</span>
           </div>
           <div className='info'>
               <span>
                   <b>Works at </b>
               </span>
-              <span>TechnoSoft Inc</span>
+              <span>{profileUser.worksAt}</span>
           </div>
           <button className='button logout-button'>Logout</button>
     </div>
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -2,46 +2,54 @@ import React from 'react'
 import Cover from '../../img/cover.jpg'
 import Profile from '../../img/profileImg.jpg'
 import './ProfileCard.css'
-const ProfileCard = () => {
-const profilePage = true; 
-  return (
-    <div className='ProfileCard'>
-          <div className='ProfileImages'>
-              <img src={Cover} alt='' />
-              <img src={Profile} alt=''/>
-          </div>
-          <div className='ProfileName'>
-              <span>Zendaya MJ</span>
-              <span>Senior UI/UX Designer</span>
-          </div>
-          <div className='followStatus'>
-              <hr />
-              <div>
-                  <div className='follow'>
-                      <span>6,890</span>
-                      <span>followings</span>
-                  </div>
-                  <div className='vl'></div>
-                  <div className='follow'>
-                      <span>1</span>
-                      <span>Followers</span>
-                  </div>
-                  {profilePage && (
-                      <>
-                          <div className='vl'></div>
-                          <div className='follow'>
-                              <span>3</span>
-                              <span>Posts</span>
-                          </div>
-                      </>
-                  )}
-              </div>
-              <hr/>
-          </div>
-          {profilePage ? "" : <span>My Profile</span>}
-          
-    </div>
-  )
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const ProfileCard = ({location}) => {
+    const user = useSelector((state) => state.authReducer.authData)
+    console.log(user.followers)
+    const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+    const posts = useSelector((state) => state.postReducer.posts)
+   
+    return (
+        <div className='ProfileCard'>
+            <div className='ProfileImages'>
+                <img src={user.coverPicture ? user.coverPicture : serverPublic + "defaultCover.jpg"} alt='coverImage' />
+                <img src={user.profilePicture ?  user.profilePicture : serverPublic + 'defaultProfile.png'} alt='profileImage' />
+            </div>
+            <div className='ProfileName'>
+                <span>{user.firstname} {user.lastname}</span>
+                <span>{user.worksAt ? user.worksAt : 'Write about yourself'}</span>
+            </div>
+            <div className='followStatus'>
+                <hr />
+                <div>
+                    <div className='follow'>
+                        <span>{user.following.length}</span>
+                        <span>followings</span>
+                    </div>
+                    <div className='vl'></div>
+                    <div className='follow'>
+                        <span>{user.followers.length}</span>
+                        <span>Followers</span>
+                    </div>
+                    {/* for profile page */}
+                    {location === "profilePage" && (
+                        <>
+                            <div className='vl'></div>
+                            <div className='follow'>
+                                <span>{ posts.filter((post)=> post.userId === user._id).length}</span>
+                                <span>Posts</span>
+                            </div>
+                        </>
+                    )}
+                </div>
+                <hr />
+            </div>
+            {location === "profilePage" ? ("" ): (<span><Link to = {`/profile/${user._id}`} style={{ textDecoration: "none", color: "inherit" }}>My Profile</Link></span>)}
+
+        </div>
+    )
 }
 
 export default ProfileCard
